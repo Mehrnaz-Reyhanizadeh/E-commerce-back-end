106b13f8047886eb819eb98b1953d80464f2e47e
Reyhanizadeh <-->
2022-12-18 14:47:01 +0330

    cors package has been added

--- src/models/product.js
+++ src/models/product.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const timestamp = require("mongoose-timestamp");
+// const timestamp = require("mongoose-timestamp");
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
--- startup/config.js
+++ startup/config.js
@@ -1,4 +1,6 @@
+const cors = require("cors");
 module.exports = function (app, express) {
+  app.use(cors());
   app.use(express.json());
   app.use(express.static("public"));
   app.use(express.urlencoded({ extended: true }));
